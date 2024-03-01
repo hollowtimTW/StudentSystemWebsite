b66aa1ec96fcb9d77c784528e41a90d9dc742f1b
子宜 莊 <qj92724275@gmail.com>
2024-03-01 23:56:42 +0800

    訪客前台 資料出來=> next 把版套好

--- text_loginWithBackgrount/Areas/course_management/Controllers/courseController.cs
+++ text_loginWithBackgrount/Areas/course_management/Controllers/courseController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Class_system_Backstage_pj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace text_loginWithBackgrount.Areas.course_management.Controllers
 {
@@ -8,9 +10,29 @@ namespace text_loginWithBackgrount.Areas.course_management.Controllers
     [Area("course_management")]
     public class courseController : Controller
     {
+        private readonly studentContext _context;
+
+        public courseController(studentContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> HomeIndexCard()
+        {
+            var now = DateTime.Now;
+            var sixMonthsLater = now.AddMonths(6);
+
+            var studentContext = _context.T課程班級s
+                .Include(t => t.班級導師)
+                .Where(t => t.入學日期 > now && t.入學日期 <= sixMonthsLater)
+                .Take(3);
+
+            return PartialView("_HomeIndexCardPartial", await studentContext.ToListAsync());
         }
     }
 }
