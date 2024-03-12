c70a9a333bed135edf1ae6b8847ea56ee1871fc4
weiming <hubinshin@gmail.com>
2024-03-12 09:54:12 +0800

    篩選

---
+++ text_loginWithBackgrount/Areas/video_management/Controllers/ShoppingCartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace text_loginWithBackgrount.Areas.video_management.Controllers
+{
+    public class ShoppingCartController : Controller
+    { /// <summary>
+      /// 學生登入後會看到的顯示頁面
+      /// </summary>
+        [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "student")]
+        [Area("video_management")]
+        public IActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
