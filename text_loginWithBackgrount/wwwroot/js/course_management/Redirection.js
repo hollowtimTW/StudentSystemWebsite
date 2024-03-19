538b459bc84183114639917be45719b8925c6a8d
子宜 莊 <qj92724275@gmail.com>
2024-03-19 10:48:23 +0800

    js提交2

--- text_loginWithBackgrount/wwwroot/js/course_management/Redirection.js
+++ text_loginWithBackgrount/wwwroot/js/course_management/Redirection.js
@@ -1,1 +1,9 @@
-﻿
+﻿function classFeature(id) {
+    window.location.href = `/course_management/courseHome/classFeature/${id}`;
+
+}
+function whiteBoard(id) {
+    window.location.href = `/course_management/courseHome/whiteBoard/${id}`;
+
+}
+
