334a3763a0cb8e129590cf2aa46f92e3567aa1e1
子宜 莊 <qj92724275@gmail.com>
2024-03-20 13:42:13 +0800

    1.新增班及功能的註解 BUG 處理完成 2.把演示的BUTTON加入

--- text_loginWithBackgrount/Areas/course_management/Controllers/T班級創建Controller.cs
+++ text_loginWithBackgrount/Areas/course_management/Controllers/T班級創建Controller.cs
@@ -24,17 +24,118 @@ namespace Class_system_Backstage_pj.Areas.course_management.Controllers
             _context = context;
         }
 
-       
 
-        //GET: course_management/T課程班級/Create
+
+        //course_management/T班級創建/ [read]
+        // <summary>
+        // 這個方法用於呈現科目管理系統中的班級新增的子功能頁面。
+        // </summary>
+        // <returns>成功返回該頁面</returns>
         public IActionResult Index()
         {
-            ViewData["班級導師id"] = new SelectList(_context.T會員老師s, "老師id", "姓名");          
-            ViewBag.course = courseIndex();
-            return View();
+            try
+            {
+                //如果為null，前端負責處理
+                ViewData["班級導師id"] = TeacherInfo();
+                ViewBag.course = courseIndex();
+                return View();
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"發生錯誤: {ex.Message}");
+                return View("Errors");
+            }
         }
 
-        // POST: course_management/T課程班級/Create
+        //course_management/T班級創建/ [api]
+        // <summary>
+        // 這個方法用於返回科目管理系統中的班級新增的子功能頁面所需要的科目資料select對象。
+        // </summary>
+        // <returns>成功返回SelectList對象</returns>
+        public SelectList courseIndex()
+        {
+            try
+            {
+                SelectList coursesSelectList = new SelectList(_context.T課程科目s.Select(c => new
+                {
+                    coursesid = c.科目id,
+                    coursesName = c.科目名稱
+                }), "coursesid", "coursesName");
+
+
+                return coursesSelectList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"發生錯誤: {ex.Message}");
+                return null;
+            }
+
+        }
+
+        //course_management/T班級創建/ [api]
+        // <summary>
+        // 這個方法用於返回科目管理系統中的班級新增的子功能頁面所需要的老師資料select對象。
+        // </summary>
+        // <returns>成功返回SelectList對象</returns>
+        public SelectList TeacherInfo()
+        {
+            try
+            {
+                SelectList teacherSelectList = new SelectList(_context.T會員老師s, "老師id", "姓名");
+
+                return teacherSelectList;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"發生錯誤: {ex.Message}");
+                return null;
+            }
+
+        }
+
+        //course_management/T班級創建/ [api]
+        // <summary>
+        // 這個方法用於返回科目管理系統中的班級新增的子功能頁面所指定傳來的科目id能夠授課的老師資料。
+        // </summary>
+        // <param name="id">T課程科目id。</param>
+        // <returns>成功返回SelectList對象</returns>
+        public async Task<IActionResult> GetTeachersBySubjectId(int? id)
+        {
+            try
+            {
+                if (id == null || id == 0 || _context.T課程科目s == null)
+                {
+                    return View("Errors");
+
+                }
+                var t課程科目 = await _context.T課程科目s
+                   .FirstOrDefaultAsync(m => m.科目id == id);
+
+                if (t課程科目 == null)
+                {
+                    return View("Errors");
+
+                }
+                var Courseteachers = await _context.T課程老師科目s
+                .Where(tc => tc.科目id == id)
+                .Select(tc => tc.老師)
+                .ToListAsync();
+                return Json(Courseteachers);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"發生錯誤: {ex.Message}");
+                return View("Errors");
+            }
+        }
+
+        // POST: course_management/T班級創建/Create
+        // <summary>
+        // 這個方法用於處理科目管理系統中的班級新增的子功能頁面create所指定傳來的表單資料。
+        // </summary>
+        // <returns>成功返回SelectList對象</returns>
         [HttpPost]
         public async Task<IActionResult> Create()
         {
@@ -48,13 +149,14 @@ namespace Class_system_Backstage_pj.Areas.course_management.Controllers
 
                 if(data == null)
                 {
-                    return NotFound();
+                    return View("Errors");
                 }
-                // 將數據存入資料庫
+                // 將數據存入資料庫，因為有多張表要處理 所以使用Transaction
                 using (var transaction = await _context.Database.BeginTransactionAsync())
                 {
                     try
                     {
+                        //step1:先存入班級表
                         var t課程班級 = new T課程班級
                         {
                             班級名稱 = data.班級名稱,
@@ -67,7 +169,10 @@ namespace Class_system_Backstage_pj.Areas.course_management.Controllers
 
                         await _context.SaveChangesAsync(); 
 
+                        //step2:將剛剛新增的班級的ID拿來和表單裡的班級科目[data.班級科目]一起再存入班級科目表
                         var 班級id = t課程班級.班級id;
+
+                        // 如果有選擇班級科目，則將其存入班級科目表
                         if (data.班級科目 != null && 班級id != 0)
                         {
                             foreach (var subjectTeacherPair in data.班級科目)
@@ -87,65 +192,34 @@ namespace Class_system_Backstage_pj.Areas.course_management.Controllers
                                 
                             }
 
-                            await _context.SaveChangesAsync(); 
+                            await _context.SaveChangesAsync();
                         }
-
+                        // 提交事務，確認所有的變更
                         await transaction.CommitAsync();
+                        return RedirectToAction("Index", "T課程班級", new { area = "course_management" });
+
                     }
                     catch (Exception ex)
                     {
-                        await transaction.RollbackAsync(); // 回滾事務
+                        // 發生錯誤時回滾事務
+                        await transaction.RollbackAsync(); 
                         Console.WriteLine(ex);
-                        throw;
+                        return View("Errors");
                     }
                 }
-                return RedirectToAction(nameof(Index));
 
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
-                return StatusCode(500, "Internal server error: " + ex.Message); // 處理全局異常
+                return View("Errors");
             }
         }
 
 
-        //GET: course_management/T課程科目
-        public SelectList courseIndex()
-        {
-            SelectList coursesSelectList = new SelectList(_context.T課程科目s.Select(c => new
-            {
-                coursesid = c.科目id,
-                coursesName = c.科目名稱
-            }), "coursesid", "coursesName");
 
 
-            return coursesSelectList;
-
-        }
-
-        public async Task<IActionResult> GetTeachersBySubjectId(int? id)
-        {
-            if (id == null || _context.T課程科目s == null)
-            {
-                return NotFound();
-            }
-            var t課程科目 = await _context.T課程科目s
-               .FirstOrDefaultAsync(m => m.科目id == id);
-
-            if (t課程科目 == null)
-            {
-                return NotFound();
-            }
-            var Courseteachers = await _context.T課程老師科目s
-            .Where(tc => tc.狀態 == 1 && tc.科目id == id)
-            .Select(tc => tc.老師)
-            .ToListAsync();
-
-
-
-            return Json(Courseteachers);
-        }
+        
     }
 }
---
+++ text_loginWithBackgrount/wwwroot/js/course_management/CreateClass.js
@@ -0,0 +1,162 @@
+﻿$(document).ready(function () {
+    //全域變數，代表被選到的班級科目
+    var selectedSubjects = [];
+
+    // step1的button，不會送資訊到後端，會檢查每個欄位
+    $(document).on('click', '#next', function (e) {
+
+        e.preventDefault();
+
+        var className = $("[data-id='班級名稱']").val();
+        var teacherId = $("[data-id='班級導師id']").val();
+        var admissionDate = $("[data-id='入學日期']").val();
+        var graduationDate = $("[data-id='結訓日期']").val();
+
+        if (className == "" || teacherId == "" || admissionDate == "" || graduationDate == "") {
+            alert("請填寫所有字段。");
+            return false;
+        } else if (admissionDate >= graduationDate) {
+            alert("請正確填寫時間。");
+            return false;
+        } else {
+            $("#progressBar").css("width", "50%");
+            $("#title").text("step2:選擇班級科目");
+            $("#step1").hide();
+            $("#step2").show();
+        }
+    });
+
+    // demo的button，填寫好資料
+    $(document).on('click', '#demo', function (e) {
+
+        e.preventDefault();
+
+        $("[data-id='班級名稱']").val("msitDemoTest01");
+        $("[data-id='班級導師id']").val("1");
+        $("[data-id='入學日期']").val("2024-04-20T00:00");
+        $("[data-id='結訓日期']").val("2024-09-20T00:00");       
+    });
+
+    //step2的button，和後端互動的表單由此送出
+    $("#submit").on('click', function (e) {
+        e.preventDefault();
+
+        var formData = {
+            班級名稱: $("[data-id='班級名稱']").val(),
+            班級導師id: $("[data-id='班級導師id']").val(),
+            入學日期: $("[data-id='入學日期']").val(),
+            結訓日期: $("[data-id='結訓日期']").val(),
+            班級科目: selectedSubjects,
+            狀態: 1
+        };
+
+        $.ajax({
+            url: "/course_management/T班級創建/Create",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function (response) {
+                console.log("Success:", response);
+                alert("成功");
+                window.location.href = '/course_management/T課程班級/Index';
+            },
+            error: function (xhr, status, error) {
+                console.error("Error:", error);
+                alert("發生錯誤，請稍後再試。");
+            }
+        });
+    });
+
+    //取得每個科目的授課老師列表
+    $('#subjectSelect').change(function () {
+        var selectedSubjectId = $(this).val();
+        $.ajax({
+            url: '/course_management/T班級創建/GetTeachersBySubjectId/' + selectedSubjectId,
+            type: 'GET',
+            success: function (data) {
+                console.log(data);
+                if (data && data.length > 0) {
+
+                    $('#TeacherList').empty();
+                    $.each(data, function (index, teacher) {
+                        // 為每位老師創建相應的按鈕
+                        var button = $('<button>').text('選擇').addClass('btn btn-primary selectTeacher')
+                            .attr({
+                                'data-teacher-id': teacher.老師id,
+                                'data-course-id': selectedSubjectId
+                            });
+                        // 創建表格行，有兩欄，一欄顯示老師姓名和一欄選擇按鈕
+                        var row = $('<tr>').append($('<td>').text(teacher.姓名)).append($('<td>').append(button));
+                        $('#TeacherList').append(row);
+                    });
+                } else {
+                    $('#TeacherList').empty();
+                    var row = $('<tr>').append($('<td>').addClass('text-center').text("目前沒有授課老師"));
+                    $('#TeacherList').append(row);
+                }
+            },
+            error: function () {
+                alert('Failed to retrieve teachers.');
+            }
+        });
+    });
+
+
+    
+
+    //刪除陣列
+    $(document).on('click', '#subjectTeacherList button', function (e) {
+        e.preventDefault();
+        //被點擊到的就是this
+        var teacherId = $(this).closest('li').attr('data-teacher-id');
+        var courseId = $(this).closest('li').attr('data-course-id');
+        //不要回傳符合courseId和teacherId的組合
+        selectedSubjects = selectedSubjects.filter(function (item) {
+            return !(item.courseId === courseId && item.teacherId === teacherId);
+        });
+        //刪除在畫面中的組合 就是this的最近li element刪除
+        $(this).closest('li').remove();     
+    });
+
+    //新增陣列
+    $(document).on('click', '.selectTeacher', function (e) {
+        e.preventDefault();
+
+        var teacherId = $(this).attr('data-teacher-id');
+        var teacherName = $(this).closest('tr').find('td:first').text();
+        var courseId = $(this).attr('data-course-id');
+        var courseName = $('#subjectSelect option:selected').text();
+
+        // 檢查是否已經存在相同的科目和老師組合
+        var exists = selectedSubjects.some(function (item) {
+            return item.courseId === courseId && item.teacherId === teacherId;
+        });
+
+        // 如果已存在相同的組合，則不執行後續操作
+        if (exists) {
+            alert("該科目和老師組合已存在！");
+            return;
+        }
+
+        //新增新的科目-老師組合 到陣列
+        selectedSubjects.push({ courseId: courseId, teacherId: teacherId });
+
+        //渲染畫面
+        //step1 設置好button
+        var button = $('<button>').text('刪除').addClass('btn btn-warning');
+        //step2 先設置li元素並且加入屬性讓之後能取得老師id和課程id，1並且apend顯示的span 顯示科目名稱和老師名稱
+        var listItem = $('<li>').addClass('list-group-item d-flex justify-content-between align-items-center')
+            .attr({
+                'data-teacher-id': teacherId,
+                'data-course-id': courseId
+            })
+            .append($('<span>').addClass('fs-6 fw-bold text-muted w-35').text(courseName))
+            .append($('<span>').addClass('fs-6 w-35').text(teacherName))
+            .append(button);
+
+        //新增到list裡
+        $('#subjectTeacherList').append(listItem);
+    });
+
+    
+});
