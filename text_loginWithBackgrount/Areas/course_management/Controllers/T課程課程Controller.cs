a76e62d09be1cd002e043ead449aac295368dbf5
子宜 莊 <qj92724275@gmail.com>
2024-03-25 03:26:09 +0800

    課程新增相關

--- text_loginWithBackgrount/Areas/course_management/Controllers/T課程課程Controller.cs
+++ text_loginWithBackgrount/Areas/course_management/Controllers/T課程課程Controller.cs
@@ -9,6 +9,8 @@ using Class_system_Backstage_pj.Models;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using text_loginWithBackgrount.Areas.course_management.ViewModel.time;
+using System.Globalization;
 
 namespace Class_system_Backstage_pj.Areas.course_management.Controllers
 {
@@ -26,80 +28,108 @@ namespace Class_system_Backstage_pj.Areas.course_management.Controllers
         // GET: course_management/T課程課程
         public async Task<IActionResult> classCourseTimeIndex(int? id)
         {
-            var TimeContext = _context.T課程班級科目s
-            .Include(t => t.T課程課程s)
-            .Where(t => t.班級id == id)
-            .SelectMany(subject => subject.T課程課程s);
-
-            var courseContext = _context.T課程班級科目s
-                .Include(t => t.老師)
-                .Include(t => t.科目)
+            try
+            {
+                var TimeContext = _context.T課程班級科目s
+                .Include(t => t.T課程課程s)
                 .Where(t => t.班級id == id)
-                .ToList();
+                .SelectMany(subject => subject.T課程課程s);
 
-            var classContext = _context.T課程班級s
-                .Where(t => t.班級id == id)
-                .FirstOrDefault();
+                var courseContext = _context.T課程班級科目s
+                    .Include(t => t.老師)
+                    .Include(t => t.科目)
+                    .Where(t => t.班級id == id)
+                    .ToList();
 
-            ViewBag.Class = classContext;
-            ViewBag.ClassCourse = courseContext;
+                var classContext = _context.T課程班級s
+                    .Where(t => t.班級id == id)
+                    .FirstOrDefault();
 
-            return View(TimeContext);
+                ViewBag.Class = classContext;
+                ViewBag.ClassCourse = courseContext;
+
+                return View(TimeContext);
+            }catch(Exception ex)
+            {
+                Console.WriteLine("error :" + ex.Message);
+                return View("Errors");
+            }
         }
 
         // GET: course_management/T課程課程/Create
         [HttpGet, ActionName("Create")]
-        public IActionResult Create(int? id)
+        public IActionResult Create(int? id,string? date)
         {
             ViewBag.CourseData = id;
+            ViewBag.TimeData = date;
             return PartialView("_CreatePartial");
         }
 
         // POST: T課程課程/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("班級科目id,開始時間,結束時間,課堂摘要,值日生id,狀態")] T課程課程 t課程課程)
+        public async Task<IActionResult> Create([Bind("班級科目id,開始時間,結束時間,課堂摘要,值日生id,狀態,日期")] CourseTimeDto model)
         {
-            if (ModelState.IsValid)
+            try
             {
-                if (t課程課程.開始時間 == null || t課程課程.結束時間 == null)
+                DateTime date = DateTime.ParseExact(model.日期, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+
+                DateTime combinedStartTime = date.Add(model.開始時間.TimeOfDay);
+                DateTime combinedEndTime = date.Add(model.結束時間.TimeOfDay);
+
+                if (combinedStartTime == null || combinedEndTime == null)
                 {
                     ModelState.AddModelError(string.Empty, "開始時間和結束時間必填。");
                     return BadRequest(ModelState);
                 }
 
-                bool isOverlap = _context.T課程課程s
-                .Any(c =>
-                (c.開始時間 <= t課程課程.開始時間 && t課程課程.開始時間 <= c.結束時間) ||
-                (c.開始時間 <= t課程課程.結束時間 && t課程課程.結束時間 <= c.結束時間));
-                
+                bool isOverlap = _context.T課程課程s.Any(c =>
+                     c.班級科目id == model.班級科目id &&
+                     ((c.開始時間 < combinedEndTime && combinedStartTime < c.結束時間) ||
+                     (combinedStartTime <= c.開始時間 && c.開始時間 < combinedEndTime)));
+
+
                 if (isOverlap)
                 {
-                     ModelState.AddModelError(string.Empty, "新的課程和舊有的資料有重疊請重新。");
+                    ModelState.AddModelError(string.Empty, "新的課程和舊有的資料有重疊請重新。");
                     return BadRequest(ModelState);
                 }
 
 
                 var 班級 = _context.T課程班級s
                 .Include(t => t.T課程班級科目s)
-                .FirstOrDefault(t => t.T課程班級科目s.Any(s => s.班級科目id == t課程課程.班級科目id));
+                .FirstOrDefault(t => t.T課程班級科目s.Any(s => s.班級科目id == model.班級科目id));
 
-                if (t課程課程.開始時間 < 班級.入學日期 || t課程課程.結束時間 > 班級.結訓日期)
+                if (combinedStartTime < 班級.入學日期 || combinedEndTime > 班級.結訓日期)
                 {
                     ModelState.AddModelError(string.Empty, "新的課程時間不在開班時間內。");
                     return BadRequest(ModelState);
                 }
+                var t課程課程 = new T課程課程
+                {
+                    班級科目id = model.班級科目id,
+                    開始時間 = combinedStartTime,
+                    結束時間 = combinedEndTime,
+                    課堂摘要 = model.課堂摘要,
+                    值日生id = model.值日生id,
+                    狀態 = model.狀態,
+                };
 
                 _context.Add(t課程課程);
                 await _context.SaveChangesAsync();
+
+
+                var id = _context.T課程班級科目s
+                    .Where(t => t.班級科目id == model.班級科目id)
+                    .Select(t => t.班級id)
+                    .FirstOrDefault();
+
+                return RedirectToAction(nameof(classCourseTimeIndex), new { id });
+            }catch(Exception ex)
+            {
+                Console.WriteLine("error :" + ex.Message);
+                return View("Errors");
             }
-
-            var id=_context.T課程班級科目s           
-                .Where(t => t.班級科目id == t課程課程.班級科目id)
-                .Select(t=>t.班級id)
-                .FirstOrDefault();
-
-            return RedirectToAction(nameof(classCourseTimeIndex),new { id });
 
         }
 
---
+++ text_loginWithBackgrount/Areas/course_management/ViewModel/time/CourseTimeDto.cs
@@ -0,0 +1,23 @@
+﻿using Class_system_Backstage_pj.Models;
+
+namespace text_loginWithBackgrount.Areas.course_management.ViewModel.time
+{
+    public class CourseTimeDto
+    {
+
+        public int 班級科目id { get; set; }
+
+        public DateTime 開始時間 { get; set; }
+
+        public DateTime 結束時間 { get; set; }
+
+        public string 課堂摘要 { get; set; }
+
+        public int? 值日生id { get; set; }
+
+        public int 狀態 { get; set; }
+
+        public string  日期 { get; set; }
+
+    }
+}
